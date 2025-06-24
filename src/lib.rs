bb58865c50edf7fdc50480dd29244f80734c7b73
Yonatan Reicher <yony252525@gmail.com>
2025-06-24 15:45:22 +0300

    Docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+//! Provides functionality to parse and run code in a simple, interpreted PL.
+
+/// Defines the Abstract Syntax Tree.
 pub mod ast;
 pub mod value;
 pub mod parse;
+/// Defines the context of the interpreter while it is running.
 pub mod context;
+/// Evaluating ASTs to values.
 pub mod eval;
+/// Executing programs.
 pub mod execute;
