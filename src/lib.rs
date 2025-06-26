1218b8b59eb7058f11cc463c0a437135540d10e4
Yonatan Reicher <yony252525@gmail.com>
2025-06-26 12:51:40 +0300

    Add `execute_string`

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,18 @@ pub mod eval;
 /// Executing programs.
 pub mod execute;
 
+pub fn execute_string(source_code: &str) -> Result<Vec<value::Value>, Box<dyn std::error::Error>> {
+    let out = Default::default();
+    let mut context = context::Context {
+        vars: [].into(),
+        out: context::PrintOutput::Vec(std::rc::Rc::clone(&out)),
+    };
+    let ast = parse::parse(source_code)?;
+    ast.execute(&mut context);
+    let vec = std::mem::take(out.borrow_mut().as_mut());
+    Ok(vec)
+}
+
 // Re-export
 pub mod prelude {
     use super::*;
@@ -18,4 +30,5 @@ pub mod prelude {
     pub use ast::{Program, Statement, Expr, BinOp};
     pub use value::Value;
     pub use context::Context;
+    pub use super::execute_string;
 }
