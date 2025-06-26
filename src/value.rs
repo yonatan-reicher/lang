36ef124746c668e542ad5083b172ede151059710
Yonatan Reicher <yony252525@gmail.com>
2025-06-26 12:50:19 +0300

    Add From<T> impls for Value

--- src/value.rs
+++ src/value.rs
@@ -6,3 +6,16 @@ pub enum Value {
     Str(String),
 }
 
+macro_rules! impl_from {
+    ($constructor: ident, $from: ty) => {
+        impl From<$from> for Value {
+            fn from(x: $from) -> Self {
+                Self::$constructor(x)
+            }
+        }
+    };
+}
+
+impl_from!(Bool, bool);
+impl_from!(Int, i64);
+impl_from!(Str, String);
