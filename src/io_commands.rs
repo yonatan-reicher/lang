8b6197c9e9e8aa26ee15795ab25615cbc1c1ed0e
Jonathan Reicher <yony252525@gmail.com>
2025-08-26 16:52:57 +0300

    Fix bad Print commands being printed instead of erroring

--- src/io_commands.rs
+++ src/io_commands.rs
@@ -33,6 +33,19 @@ pub enum Error {
     Io(std::io::Error),
 }
 
+/// Takes a labeled value, or a label function or closure, and returns it's
+/// label and the arguments that have been applied to it.
+fn unlabel(x: &Value) -> Option<(&PLabel, &Vec<Value>)> {
+    match x {
+        Value::Labeled { label, arguments } => Some((label, arguments)),
+        Value::Func(Func::Label(LabelFunc {
+            label,
+            applied_already,
+        })) => Some((label, applied_already)),
+        _ => None,
+    }
+}
+
 pub fn execute(
     x: &Value,
     context: &Context,
@@ -47,45 +60,43 @@ pub fn execute(
         Ok(())
     }
 
-    match x.clone() {
-        Value::Labeled { label, arguments } => {
-            // TODO: Handle the case of `Print 2` (missing parameter, so it is a LabelFunc`
-            // Print
-            if label == print_label {
-                let [value, next] = arguments.as_slice() else {
-                    return Err(Error::IoValue(
-                        IoValueError::PrintDidNotHave2Parameters,
-                        x.clone(),
-                    ));
-                };
-                println!("{value}");
-                execute(next, context, input)
-            // Input
-            } else if label == input_label {
-                let [Value::Func(f)] = arguments.as_slice() else {
-                    return Err(Error::IoValue(
-                        IoValueError::InputDidNotHaveASingleFunctionParamter,
-                        x.clone(),
-                    ));
-                };
-                let mut line = String::new();
-                input.read_line(&mut line)?;
-                line = line.trim_end_matches("\r\n").trim_end_matches("\n").into();
-                let next = f.apply(line.into())?;
-                execute(&next, context, input)
-            // None
-            } else if label == none_label {
-                if !arguments.is_empty() {
-                    return Err(Error::IoValue(
-                        IoValueError::NoneShouldHaveZeroParameters,
-                        x.clone(),
-                    ));
-                }
-                Ok(())
-            } else {
-                other_value(x)
+    if let Some((label, arguments)) = unlabel(x) {
+        // Print
+        if *label == print_label {
+            let [value, next] = arguments.as_slice() else {
+                return Err(Error::IoValue(
+                    IoValueError::PrintDidNotHave2Parameters,
+                    x.clone(),
+                ));
+            };
+            println!("{value}");
+            execute(next, context, input)
+        // Input
+        } else if *label == input_label {
+            let [Value::Func(f)] = arguments.as_slice() else {
+                return Err(Error::IoValue(
+                    IoValueError::InputDidNotHaveASingleFunctionParamter,
+                    x.clone(),
+                ));
+            };
+            let mut line = String::new();
+            input.read_line(&mut line)?;
+            line = line.trim_end_matches("\r\n").trim_end_matches("\n").into();
+            let next = f.apply(line.into())?;
+            execute(&next, context, input)
+        // None
+        } else if *label == none_label {
+            if !arguments.is_empty() {
+                return Err(Error::IoValue(
+                    IoValueError::NoneShouldHaveZeroParameters,
+                    x.clone(),
+                ));
             }
+            Ok(())
+        } else {
+            other_value(x)
         }
-        _ => other_value(x),
+    } else {
+        other_value(x)
     }
 }
